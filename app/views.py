0f67697e573a2243712d3b67cecdce5d7a252af1
muslimbek77 <muslimbekbaratov57@gmail.com>
2025-09-18 16:25:22 +0500

    final templates fix

--- app/views.py
+++ app/views.py
@@ -7,3 +7,30 @@ def home_page(request):
 
 def blog_page(request):
     return render(request=request,template_name="blog.html")
+
+def category_page(request):
+    return render(request=request,template_name="category.html")
+
+def checkout_page(request):
+    return render(request=request,template_name="checkout.html")
+
+def confirmation_page(request):
+    return render(request=request,template_name="confirmation.html")
+
+def cart_page(request):
+    return render(request=request,template_name="cart.html")
+
+def contact_page(request):
+    return render(request=request,template_name="contact.html")
+
+def single_product_page(request):
+    return render(request=request,template_name="single-product.html")
+
+def login_page(request):
+    return render(request=request,template_name="login.html")
+
+def register_page(request):
+    return render(request=request,template_name="register.html")
+
+def tracking_order_page(request):
+    return render(request=request,template_name="tracking-order.html")
