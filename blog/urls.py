85c2f4004b958744be919054ee211e9e8a5ebae7
muslimbek77 <muslimbekbaratov57@gmail.com>
2025-10-22 21:23:37 +0500

    djago slug and hitcount

--- blog/urls.py
+++ blog/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import BlogPostView
+from .views import BlogPostView,PostDetailView
 
 urlpatterns = [
-    path('',BlogPostView.as_view(),name='blog-page')
+    path('',BlogPostView.as_view(),name='blog-page'),
+    path('<int:pk>/',PostDetailView.as_view(),name='blog-detail-view')
     
 ]
--- config/settings.py
+++ config/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    
     'jazzmin',
     'django.contrib.admin',
     'django.contrib.auth',
@@ -42,7 +43,8 @@ INSTALLED_APPS = [
     'account',
     'blog',
     'order',
-    'product'
+    'product',
+    'hitcount',
 ]
 
 MIDDLEWARE = [
